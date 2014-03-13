cda2ccac93efdab1cbce2e0964fb37236ab54383
jiangjiawen <jwjiang@lewatek.com>
2014-03-13 20:31:24 +0800

    upload weekly update(20140313)

--- jellybean42.mk
+++ jellybean42.mk
@@ -1,79 +1,83 @@
-private-lewa-apps := LewaAppStore \
-					 Bluetooth \
-					 LewaCit \
-					 CalendarProvider \
-					 ContactsProvider \
-					 LewaDeskClock \
-					 DownloadProvider \
-					 DownloadProviderUi \
-					 LewaFlashlight \
-					 LewaGameCenter \
-					 LewaIntercept \
-					 LewaLabiSync \
-					 LatinIME \
-					 LewaLauncherX \
-					 LewaAntitheft \
-					 LewaCalculator \
-					 LewaDataUsageAdjust \
-					 LewaDefaultTheme \
-					 LewaFileManager \
-					 LewaLockScreen \
-					 LewaNetworkManager \
-					 LewaPackageInstaller \
-					 LewaPermManager \
-					 LewaPond \
-					 LewaPush \
-					 LewaSecurity2 \
-					 LewaSoundRecorder \
-					 LewaSystemClean \
-					 LewaSystemCleanService \
-					 LewaUpdater \
-					 LewaWeather \
-					 LocationProvider \
-					 MmsFavoriteProvider \
-					 MediaProvider \
-					 NetworkLocation \
-					 Phone \
-					 Provision \
-					 PIM \
-					 LewaPower+ \
-					 LewaQrcodeScanner \
-					 LewaSearch \
-					 Settings \
-					 SettingsProvider \
-					 SystemUI \
-					 TelephonyProvider \
-					 LewaThemeChooser \
-					 LewaThemeManager \
-					 LewaVirusDefense \
-					 FusedLocation \
-					 LewaPlayer \
-					 LewaCompass \
-					 Email \
-                     LewaSensorProvider
+private-lewa-apps := \
+            LewaAppStore \
+            Bluetooth \
+            LewaCit \
+            CalendarProvider \
+            ContactsProvider \
+            LewaDeskClock \
+            DownloadProvider \
+            DownloadProviderUi \
+            LewaFlashlight \
+            LewaGameCenter \
+            LewaIntercept \
+            LewaLabiSync \
+            LatinIME \
+            LewaLauncherX \
+            LewaAntitheft \
+            LewaCalculator \
+            LewaDataUsageAdjust \
+            LewaDefaultTheme \
+            LewaFileManager \
+            LewaLockScreen \
+            LewaNetworkManager \
+            LewaPackageInstaller \
+            LewaPermManager \
+            LewaPond \
+            LewaPush \
+            LewaSecurity2 \
+            LewaSoundRecorder \
+            LewaSystemClean \
+            LewaSystemCleanService \
+            LewaUpdater \
+            LewaWeather \
+            LocationProvider \
+            MmsFavoriteProvider \
+            MediaProvider \
+            NetworkLocation \
+            Phone \
+            Provision \
+            PIM \
+            LewaPower+ \
+            LewaQrcodeScanner \
+            LewaSearch \
+            Settings \
+            SettingsProvider \
+            SystemUI \
+            TelephonyProvider \
+            LewaThemeChooser \
+            LewaThemeManager \
+            LewaVirusDefense \
+            FusedLocation \
+            LewaPlayer \
+            LewaCompass \
+            Email \
+            LewaSensorProvider
 
-private-google-frameworks := OneTimeInitializer \
-					 NetworkLocation \
-					 MediaUploader \
-					 ChromeBookmarksSyncAdapter \
-					 GoogleBackupTransport \
-					 GoogleCalendarSyncAdapter \
-					 GoogleContactsSyncAdapter \
-					 GoogleFeedback \
-					 GoogleLoginService \
-					 GooglePartnerSetup \
-					 GoogleServicesFramework \
-					 GoogleTTS \
-					 Microbes
+private-google-frameworks := \
+            OneTimeInitializer \
+            NetworkLocation \
+            MediaUploader \
+            ChromeBookmarksSyncAdapter \
+            GoogleBackupTransport \
+            GoogleCalendarSyncAdapter \
+            GoogleContactsSyncAdapter \
+            GoogleFeedback \
+            GoogleLoginService \
+            GooglePartnerSetup \
+            GoogleServicesFramework \
+            GoogleTTS \
+            Microbes
 
-private-preinstall-apps := BaiduMap \
-					 Calendar365 \
-					 GNotes \
-					 iReader \
-					 VoiceAssistant
+private-preinstall-apps := \
+            BaiduMap \
+            Calendar365 \
+            GNotes \
+            iReader \
+            VoiceAssistant
 
-private-lewa-jars := android.policy \
-					 framework \
-					 framework_ext \
-					 telephony-common \
-					 services
+private-lewa-jars := \
+            android.policy \
+            framework \
+            framework_ext \
+            telephony-common \
+            services
